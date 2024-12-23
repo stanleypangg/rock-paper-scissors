c3f8b52e99b92dc515ead31e6d5b798837960099
stanleypangg <144388332+stanleypangg@users.noreply.github.com>
2024-12-23 14:07:34 -0500

    final commit

--- script.js
+++ script.js
@@ -3,23 +3,60 @@ function getComputerChoice() {
 
     switch (randomNum) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
 function getHumanChoice() {
     let choice;
     while (true) {
-        choice = prompt("Rock, Paper or Scissors: ");
-        if (choice === "Rock" || choice === "Paper" || choice === "Scissors") {
+        choice = prompt("Rock, Paper or Scissors: ").toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
             break;
         } else {
             alert("Invalid choice");
         }
     }
-    return;
+    return choice;
 }
+
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+
+
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        if (
+            humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice == "paper" ||
+            humanChoice === "paper" && computerChoice == "rock"
+        ) {
+            alert(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`);
+            humanScore++;
+        } else if (
+            computerChoice === "rock" && humanChoice === "scissors" ||
+            computerChoice === "scissors" && humanChoice == "paper" ||
+            computerChoice === "paper" && humanChoice == "rock"
+        ) {
+            alert(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`);
+            computerScore++;
+        } else {
+            alert("It's a tie!");
+        }
+    }
+
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    alert(`You score: ${humanScore}\nComputer score: ${computerScore}`);
+}
+
+playGame();
