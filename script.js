92e5bb7dc3c314a910c9c834193eebefc97d8db3
stanleypangg <144388332+stanleypangg@users.noreply.github.com>
2024-12-26 03:21:13 -0500

    added rps buttons

--- script.js
+++ script.js
@@ -28,35 +28,45 @@ function capitalize(string) {
     return string[0].toUpperCase() + string.slice(1);
 }
 
-
-
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        if (
-            humanChoice === "rock" && computerChoice === "scissors" ||
-            humanChoice === "scissors" && computerChoice == "paper" ||
-            humanChoice === "paper" && computerChoice == "rock"
-        ) {
-            alert(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`);
-            humanScore++;
-        } else if (
-            computerChoice === "rock" && humanChoice === "scissors" ||
-            computerChoice === "scissors" && humanChoice == "paper" ||
-            computerChoice === "paper" && humanChoice == "rock"
-        ) {
-            alert(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`);
-            computerScore++;
-        } else {
-            alert("It's a tie!");
-        }
+function playRound(humanChoice, computerChoice) {
+    if (
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice == "paper" ||
+        humanChoice === "paper" && computerChoice == "rock"
+    ) {
+        alert(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`);
+        // humanScore++;
+    } else if (
+        computerChoice === "rock" && humanChoice === "scissors" ||
+        computerChoice === "scissors" && humanChoice == "paper" ||
+        computerChoice === "paper" && humanChoice == "rock"
+    ) {
+        alert(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`);
+        // computerScore++;
+    } else {
+        alert("It's a tie!");
     }
 
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    alert(`You score: ${humanScore}\nComputer score: ${computerScore}`);
+    // let humanScore = 0;
+    // let computerScore = 0;
+    // playRound(getHumanChoice(), getComputerChoice());
+
+    // alert(`You score: ${humanScore}\nComputer score: ${computerScore}`);
 }
 
-playGame();
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+
+});
